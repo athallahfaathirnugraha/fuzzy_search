8184cc16f9a2705e660b3554499ccfea9e12300e
athallahfaathirnugraha <athallahfaathirnugraha@gmail.com>
2024-06-08 07:49:12 +0700

    add lev_dist

--- include/fsearch.h
+++ include/fsearch.h
@@ -1,6 +1,9 @@
 #ifndef _FSEARCH_H_
 #define _FSEARCH_H_
 
-void fsearch();
+#include <stdlib.h>
+
+int lev_dist(char *a, char *b);
+void fsearch(size_t *out, size_t *out_len);
 
 #endif
--- src/fsearch.c
+++ src/fsearch.c
@@ -1,7 +1,29 @@
 #include <fsearch.h>
-#include <stdio.h>
 
-void fsearch()
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+// https://en.wikipedia.org/wiki/Levenshtein_distance#Computing_Levenshtein_distance
+int lev_dist(char *a, char *b)
+{
+    unsigned long a_len = strlen(a);
+    unsigned long b_len = strlen(b);
+
+    if (b_len == 0) return a_len;
+    else if (a_len == 0) return b_len;
+
+    if (a[0] == b[0]) return lev_dist(&a[1], &b[1]);
+
+    return 1 + min(lev_dist(&a[1], b), min(lev_dist(a, &b[1]), lev_dist(&a[1], &b[1])));
+}
+
+void fsearch(size_t *out, size_t *out_len)
 {
     printf("fsearch\n");
 }
